b3026f81d51df0cddbf5947a985ad7439147284e
Yi Liu <purplesky8985@gmail.com>
2015-10-27 16:49:41 -0500

    server side query handler works

---
+++ hw8/app.js
@@ -0,0 +1,53 @@
+/**
+ * Created by YiLIU on 10/25/15.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var url = require('url');
+var http = require('http');
+var requestHandler = require('./requestHandler');
+
+const PORT = 8000;
+
+var server = http.createServer(function (request, response) {
+    let pathname = url.parse(request.url).pathname;
+    let queries = url.parse(request.url).query;
+    if (pathname === "/" && (queries == null || queries.length == 0)) {
+        requestHandler.handleHome(function(err, html) {
+            if (err) {
+                return console.error(err);
+            }
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(html);
+            response.end();
+        });
+    } else if (pathname === "/api/weather") {
+        requestHandler.handleQuery(queries, function(err, weather) {
+            if (err) {
+                return console.error(err);
+            }
+            response.writeHead(200, {"Content-Type": "application/json"});
+            response.write(weather);
+            response.end();
+        });
+    } else if (pathname === "/api/image") {
+        requestHandler.handleImage(queries, function(err, image) {
+            if (err) {
+                return console.error(err);
+            }
+            response.writeHead(200, {"Content-Type": "image/png"});
+            response.write(image);
+            response.end();
+        })
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
+});
+
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
+
+
---
+++ hw8/requestHandler.js
@@ -0,0 +1,199 @@
+/**
+ * Created by YiLIU on 10/26/15.
+ */
+
+'use strict';
+
+const GOOGLE_KEY= "AIzaSyDdhosspZ6NYYHaBIDtMsLtPBGD-j5FRGU";
+var fs = require('fs');
+var https = require('https');
+var xml2js = require('xml2js');
+var querystring = require('querystring');
+
+exports.handleImage = function(query, callback) {
+    queryToDict(query, function(err, infoDict) {
+        if (err) {
+            return callback(err);
+        }
+        fs.readFile("./images/" + infoDict.fileName, function(err, data) {
+            if (err) {
+                return callback(err);
+            }
+            return callback(null, data);
+        });
+
+    });
+
+};
+
+exports.handleHome = function(callback) {
+    fs.readFile("./index.html", 'utf-8', function(err, data) {
+        if (err) {
+            return callback(err);
+        }
+        return callback(null, data);
+    });
+};
+
+exports.handleQuery = function(queries, callback) {
+    queryToDict(queries, function(err, infoDict) {
+        if (err) {
+            return callback(err);
+        }
+
+        queryGeocode(infoDict, function(err, location) {
+            if (err) {
+                return callback(err);
+            }
+
+            queryForecast(infoDict, location, function(err, weather) {
+                if (err) {
+                    return callback(err);
+                }
+
+                //showHTML(infoDict, weather, function(err, HTML) {
+                //    if (err) {
+                //        return callback(err);
+                //    }
+                //
+                //    return callback(null, HTML);
+                //});
+
+                return callback(null, weather);
+            });
+        });
+    });
+};
+
+var queryToDict = function(queries, callback) {
+    var dict = {};
+    var list = queries.split('&');
+    for (var query of list) {
+        var keyValue = query.split('=');
+        dict[keyValue[0]] = keyValue[1];
+    }
+
+    return callback(null, dict);
+};
+
+var queryGeocode = function(infoDict, callback) {
+    let geoUrl = "https://maps.googleapis.com/maps/api/geocode/xml?address=" + infoDict.streetAddress + "," +
+        infoDict.city + "," + infoDict.state + "&key=" + GOOGLE_KEY;
+
+    https.get(geoUrl, function(res) {
+        var xmlRes = ""
+
+        console.log("Google Geocode responseSatus: ", res.statusCode);
+
+        res.on('data', function(data) {
+            xmlRes += data.toString();
+        });
+
+        res.on('end', function() {
+            xml2js.parseString(xmlRes, function(err, result) {
+                return callback(null, result.GeocodeResponse.result[0].geometry[0].location[0]);
+            });
+        });
+
+    }).on("error", function(err) {
+        return callback(err);
+    });
+};
+
+var queryForecast = function(infoDict, location, callback) {
+    let fcUrl = "https://api.forecast.io/forecast/473660d9a99fc4416b3d36a8a93b7ad7/" + location.lat[0] + "," +
+        location.lng[0] + "?units=" + infoDict.degreeType + "&exclude=flags";
+    let jsonStr = "";
+    https.get(fcUrl, function(res) {
+        console.log("Forecast.io responseSatus: ", res.statusCode);
+        res.on('data', function(data) {
+            jsonStr += data;
+        });
+
+        res.on('end', function() {
+            //let weather = JSON.parse(jsonStr);
+            callback(null, jsonStr);
+        });
+    }).on("error", function(err) {
+        return callback(err);
+    });
+};
+
+var showHTML = function(infoDict, weather, callback) {
+    fs.readFile("./weather.html", "utf-8", function(err, data) {
+        if (err) {
+            return callback(err);
+        }
+        let icon = getIcon(weather.currently.icon);
+        //noinspection JSValidateTypes
+        let temperature = weather.currently.temperature + (infoDict.degreeType === "us"? " Fº": " Cº");
+        let condition = weather.currently.summary;
+
+        let e = [
+            getPrecipitation(weather.currently.precipIntensity),
+            (weather.currently.precipProbability * 100) + "%",
+            parseInt(weather.currently.windSpeed) + "mph",
+            parseInt(weather.currently.dewPoint),
+            (weather.currently.humidity * 100) + "%",
+            parseInt(weather.currently.visibility) + "mi",
+            getTime(weather.daily.data[0].sunriseTime),
+            getTime(weather.daily.data[0].sunsetTime)
+        ];
+
+        let HTML = String.format(data, e[0], e[1], e[2], e[3], e[4], e[5], e[6], e[7], icon, condition, temperature);
+        return callback(null, HTML);
+    });
+};
+
+var getPrecipitation = function(pi) {
+    if (pi === 0) {
+        return "None";
+    } else if (pi === 0.002) {
+        return "Very Light";
+    } else if (pi === 0.017) {
+        return "Light";
+    } else if (pi === 0.1) {
+        return "Moderate";
+    } else {
+        return "Heavy";
+    }
+};
+
+var getTime = function(time) {
+    let t = new Date(time * 1000);
+    let hour = t.getHours() > 12? t.getHours() - 12: t.getHours();
+    let minute = t.getMinutes();
+    let sign = t.getHours() >= 12? "PM": "AM";
+
+    return ("00" + hour).slice(-2) + ": " + ("00" + minute).slice(-2) + " " + sign;
+};
+
+var getIcon = function(icon) {
+    switch(icon) {
+        case "clear-day": return "/images/clear.png";
+        case "clear-night": return "/images/clear_night.png";
+        case "rain": return "/images/rain.png";
+        case "snow": return "/images/snow.png";
+        case "sleet": return "/images/sleet.png";
+        case "wind": return "/images/wind.png";
+        case "fog": return "/images/fog.png";
+        case "cloudy": return "/images/cloudy.png";
+        case "partly-cloudy-day": return "/images/cloud_day.png";
+        case "partly-cloudy-night": return "images/cloud_night.png";
+        default: return "/#";
+    }
+};
+
+String.format = function(src){
+
+    if (arguments.length == 0) return null;
+
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    return src.replace(/\{(\d+)\}/g, function(m, i){
+
+        return args[i];
+
+    });
+
+};
